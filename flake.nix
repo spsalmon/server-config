ec31e64b9d5cc6298dee10e94253ca7d011e074f
spsalmon <psalmonsacha@gmail.com>
2024-04-16 08:51:14 +0200

    changed flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,27 @@
 {
-  description = "Nixos config flake";
+  description = "Media server Nix flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixarr.url = "github:rasmus-kirk/nixarr";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, nixarr, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-    
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [ 
-            ./hosts/default/configuration.nix
-            nixarr.nixosModules.default
-            inputs.home-manager.nixosModules.default
-          ];
-        };
+  outputs = { 
+    nixpkgs,
+    nixarr,
+    ...
+  }@inputs: {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
 
+        modules = [
+          ./hosts/default/configuration.nix
+          nixarr.nixosModules.default
+        ];
+
+        specialArgs = { inherit inputs; };
+      };
     };
+  };
 }
