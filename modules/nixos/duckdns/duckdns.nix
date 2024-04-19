efd13e05737169a854a0ea133ce7634bc9d7db01
spsalmon <psalmonsacha@gmail.com>
2024-04-19 08:50:23 +0200

    trying to add godns as a systemd service

--- modules/nixos/duckdns/duckdns.nix
+++ modules/nixos/duckdns/duckdns.nix
@@ -1,8 +1,19 @@
 { lib, config, pkgs, ... }:
 let 
-  config_path = "~/config/godns/godns_conf.json";
+  config_path = "./config.json";
 in {
   environment.systemPackages = with pkgs; [
     godns
   ];
+
+
+    systemd.services.godns = {
+      description = "GoDNS";
+      wantedBy = ["multi-user.target"];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.godns}/bin/godns -c ${config_path}";
+      };
+    };
 }
