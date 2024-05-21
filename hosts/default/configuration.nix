61936f580c409c6fc6a7add56103619bf33f309b
spsalmon <psalmonsacha@gmail.com>
2024-05-21 20:48:15 +0200

    changed setting name

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -75,7 +75,7 @@
   # Enable the OpenSSH daemon.
 
   services.openssh.enable = true;
-  services.openssh.passwordAuthentication = false;
+  services.openssh.settings.PasswordAuthentication = false;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
