115bb5d5308a01df75c512283e810f5cfc592cee
spsalmon <psalmonsacha@gmail.com>
2024-07-28 15:31:58 +0200

    added xset to turn the screen off

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -60,6 +60,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     git
+    xorg.xset
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
