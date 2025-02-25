fd8fd49dd1463e4e8f5cbe8f8a6171eae1f817ac
spsalmon <psalmonsacha@gmail.com>
2025-02-25 20:18:54 +0100

    stuff

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -1,11 +1,10 @@
+
 { config, lib, pkgs, ... }:
 {
 
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
   };
 
   # Load nvidia driver for Xorg and Wayland
